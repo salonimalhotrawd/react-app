6aa197d12f064f8a94b6714f25d5a7a454a75343
Saloni Malhotra <salonimalhotra1ind@gmail.com>
2022-04-09 01:01:17 +0530

    Add comments related to CreateElement

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ class App extends Component{
 
     /**
      * This is what the code represent during compilation process in the React.
+     * We can pass the infinite number of parameters in the createElement Method
+     * 3 Paramters is mandatory
+     * First paramter represents the HTML element
+     * Second parameter represents the Object
+     * Third parameter represents the Message to display on to the UI
      */
     // return(
     //   React.createElement('div',{className:'App'},React.createElement('h1',null, 'Hi, I/m a React app!!!!'))
